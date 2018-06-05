153f7aee04799b8480f51c39b89525c99742800a
rpriisholm <rpriisholm@hotmail.com>
2018-06-05 17:46:09 +0200

    Changes

--- StocksImportAndExport/Program.cs
+++ StocksImportAndExport/Program.cs
@@ -18,47 +18,5 @@ namespace Stocks
 
             ImportAndExport.CollectDailyData();
         }
-
-        private static void DummyCodeMultiThreading()
-        {
-            int numOfThreads = 32;
-            int currentNumOfThreads = 0;
-            WaitHandle[] waitHandles = new WaitHandle[numOfThreads];
-
-            while (false /*Needs to be replaced */)
-            {
-                EventWaitHandle handle = new EventWaitHandle(false, EventResetMode.ManualReset);
-
-                Thread exportThread = new Thread(() =>
-                {
-                    try
-                    {
-                        // Code
-                    }
-                    finally
-                    {
-                        handle.Set();
-                    }
-                });
-                exportThread.Start();
-
-                if (numOfThreads <= currentNumOfThreads)
-                {
-                    waitHandles[currentNumOfThreads] = handle;
-                    currentNumOfThreads += 1;
-                }
-                else
-                {
-                    waitHandles[currentNumOfThreads] = handle;
-                    while (WaitHandle.WaitAll(waitHandles, 10))
-                    {
-                        //Do nothing
-                    }
-
-                    waitHandles = new WaitHandle[numOfThreads];
-                    currentNumOfThreads = 0;
-                }
-            }
-        }
     }
 }
--- StocksImportAndExport/Service/ImportAndExport.cs
+++ StocksImportAndExport/Service/ImportAndExport.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Office.Interop.Excel;
 using System.IO;
+using System.Threading;
+using LumenWorks.Framework.IO.Csv;
 
 namespace Stocks.Service
 {
@@ -33,20 +35,88 @@ namespace Stocks.Service
         }
 
         public static void CollectDailyData()
-        {
-            string size = "full";
+        {           
+            //string size = "full";
             string partialPath = @"C:\StockHistory\Active\";
+           
+            #region ForEach normal
+            /*
             foreach (string symbol in GetSymbols())
             {
-                string url = "https://www.alphavantage.co/query?" + $"function=TIME_SERIES_DAILY&symbol={symbol}&outputsize={size}&apikey=11EJMYL88Q4DW5BT&datatype=csv";
+                //string url = "https://www.alphavantage.co/query?" + $"function=TIME_SERIES_DAILY&symbol={symbol}&outputsize={size}&apikey=11EJMYL88Q4DW5BT&datatype=csv";
+                string url = "https://api.iextrading.com/1.0/stock/"+ $"{symbol}/chart/5y?format=csv";
                 string path = $"{partialPath}{symbol}.csv";
 
                 //CSVToMySQL_Excel(url, path);
-                //Other.DownloadFile(url, path);
+                Other.DownloadFile(url, path);
 
                 //StreamReader streamReader = Other.GetStreamReader(url);
                 //CSVToMySQL.ExportToMySQL(streamReader, 86400, symbol);
+
+                Thread.Sleep(25);
             }
+            */
+            #endregion
+
+            Parallel.ForEach(GetSymbols(), new ParallelOptions { MaxDegreeOfParallelism = 16 }, symbol => 
+            {
+                try
+                {
+                    //string url = "https://www.alphavantage.co/query?" + $"function=TIME_SERIES_DAILY&symbol={symbol}&outputsize={size}&apikey=11EJMYL88Q4DW5BT&datatype=csv";
+                    string url = "https://api.iextrading.com/1.0/stock/" + $"{symbol}/chart/5y?format=csv";
+                    string path = $"{partialPath}{symbol}.csv";
+
+
+                    //CSVToMySQL_Excel(url, path);
+                    bool hasCompleted = false;
+                    int count = 0;
+                    int tries = 4;
+                    while (!hasCompleted && count < tries)
+                    {
+                        try
+                        {
+                            Other.DownloadFile(url, path);
+                            hasCompleted = true;
+                        }
+                        catch
+                        {
+                            count++;
+                        }
+                    }
+
+                    //StreamReader streamReader = Other.GetStreamReader(url);
+                    //CSVToMySQL.ExportToMySQL(streamReader, 86400, symbol);
+
+                    StreamReader streamReader = new StreamReader(path);
+                    CachedCsvReader csvReader = new CachedCsvReader(streamReader);
+
+                    int rows = 0;
+                    while (csvReader.ReadNextRecord())
+                    {
+                        rows++;
+
+                        if (rows >= 400)
+                        {
+                            break;
+                        }
+                    }
+                    streamReader.Close();
+
+                    if (rows < 400)
+                    {
+                        File.Delete(path);
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+            });
+
+            //Cleanup - Delete Files With Less Than 400 Rows
+           
+
             Console.WriteLine("Done");
             Console.ReadKey();
         }
@@ -58,7 +128,7 @@ namespace Stocks.Service
             StreamReader streamReader = new StreamReader(@"C:\StockHistory\StockList");
             while(streamReader.Peek() >= 0)
             {
-                symbols.Add(streamReader.ReadLine());
+                symbols.Add(streamReader.ReadLine().Trim());
             }
 
             return symbols;

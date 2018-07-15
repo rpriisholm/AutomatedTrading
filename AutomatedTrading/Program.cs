ee19c60d06cccf3dbe76db7571dd04db1e4329b4
rpriisholm <rpriisholm@hotmail.com>
2018-07-15 12:08:13 +0200

    test

--- AutomatedTrading/Program.cs
+++ AutomatedTrading/Program.cs
@@ -11,7 +11,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading;
+using System.Threading; 
 using System.Threading.Tasks;
 using TickEnum;
 using static Stocks.Services;

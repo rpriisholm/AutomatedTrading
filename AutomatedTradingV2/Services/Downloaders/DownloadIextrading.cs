bfb4234c18cde7b77f80baa14edb10b97f195a6c
rpriisholm <rpriisholm@hotmail.com>
2019-05-06 01:12:02 +0200

    download

---
+++ AutomatedTradingV2/Services/Downloaders/DownloadIextrading.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomatedTradingV2.Custom;
+using AutomatedTradingV2.Models;
+
+namespace AutomatedTradingV2.Services.Downloaders
+{
+    public class DownloadIextrading : DownloadInterface
+    {
+        public EnumeratorOnDemand<SimpleCandle, Symbol> CreateCandleLoader(string partialPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DownloadNewCandles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DownloadRowToCandles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Symbol> DownloadSymbols()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SimpleCandle> LoadCandles()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AutomatedTradingV2/Services/Downloaders/DownloadInterface.cs
@@ -0,0 +1,24 @@
+﻿using AutomatedTradingV2.Custom;
+using AutomatedTradingV2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedTradingV2.Services.Downloaders
+{
+    public interface DownloadInterface
+    {
+        List<Symbol> DownloadSymbols();
+
+        void DownloadRowToCandles();
+
+        void DownloadNewCandles();
+
+        /* Addition Means Add Row Instead Of Downloading All From Scratch */
+        EnumeratorOnDemand<SimpleCandle, Symbol> CreateCandleLoader(string partialPath);
+
+        List<SimpleCandle> LoadCandles();
+    }
+}

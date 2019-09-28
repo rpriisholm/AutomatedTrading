47998f44c1025a31ea470d4db4ae9ea8eab22aa9
rpriisholm <rpriisholm@hotmail.com>
2019-09-28 11:06:20 +0200

    Env. Args.

--- Real/RealLib/TraderLib.cs
+++ Real/RealLib/TraderLib.cs
@@ -34,7 +34,7 @@ namespace RealLib
         //Fx. public static string connectionString = @"Data Source=192.168.x.xxx;Initial Catalog=StockHistDB; User ID=myUsername;Password=myPassword;";
         //Fx. public static string connectionString = @"Data Source=localhost;Initial Catalog=StockHistDB;Integrated Security=True;";
         public static string connectionString = Environment.GetEnvironmentVariable("MSSQL_Connection_String", EnvironmentVariableTarget.Machine);
-        public static string mssqlArgs = Environment.GetEnvironmentVariable("MSSQL_ARGS", EnvironmentVariableTarget.Machine);
+        //public static string mssqlArgs = Environment.GetEnvironmentVariable("MSSQL_ARGS", EnvironmentVariableTarget.Machine);
         public static IConnection emulationConnection = new EmulationConnection(1000000, OrderLimitType.Value, 5000, 5, 100);
         public static Dictionary<string, StrategyGeneric> Strategies = new Dictionary<string, StrategyGeneric>();
         public static Dictionary<string, StrategyGeneric> ExpiringStrategies = new Dictionary<string, StrategyGeneric>();
